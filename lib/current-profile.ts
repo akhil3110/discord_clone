04667f956a13262801938d06bf08d54da394eb7e
Akhil <akhil1659@gmail.com>
2024-01-24 00:02:42 +0530

    completed server creation, server page layout

---
+++ lib/current-profile.ts
@@ -0,0 +1,24 @@
+import {auth} from "@clerk/nextjs";
+import { db } from "./db";
+
+export const currentProfile = async() => {
+
+    const {userId} = auth()
+
+    if(!userId){
+        return null;
+    }
+
+    const profile =  await db.profile.findUnique({
+        where:{
+            userId
+        }
+    })
+
+    if(!profile){
+        return null;
+    }
+
+    return profile;
+
+}
