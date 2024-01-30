f0bc98783e5610d8c500ba26789267f62bb3b687
Akhil <akhil1659@gmail.com>
2024-01-31 00:24:08 +0530

    added emoji bar in chat input

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -15,6 +15,8 @@ import { Input } from '../ui/input';
 import qs from 'query-string';
 import axios from 'axios';
 import { useModal } from '@/hooks/use-modal-store';
+import EmojiPicker from '@/components/emoji-picker';
+import { useRouter } from 'next/navigation';
 
 interface ChatInputProps {
     apiUrl: string;
@@ -35,6 +37,7 @@ const ChatInput = ({
 }: ChatInputProps) => {
 
     const {onOpen} = useModal();
+    const router = useRouter();
 
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
@@ -53,6 +56,8 @@ const ChatInput = ({
             })   
 
             await axios.post(url, values);
+            form.reset();
+            router.refresh();
 
         } catch (error) {
             console.log(error);
@@ -85,7 +90,9 @@ const ChatInput = ({
                                     <div
                                         className=' absolute top-7 right-8'
                                     >
-                                        <Smile />
+                                       <EmojiPicker 
+                                            onChange={(emoji: string) => field.onChange(`${field.value} ${emoji}`)}
+                                       />
                                     </div>
                                 </div>
                             </FormControl>
